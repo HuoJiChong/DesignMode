1f72fd8acb099938926289ec507dd64620947f80
Machals <909068353@qq.com>
2019-09-12 18:35:08 +0800

    事件总线测试

--- app/src/main/java/com/derek/app/MainActivity.java
+++ app/src/main/java/com/derek/app/MainActivity.java
@@ -1,55 +1,49 @@
 package com.derek.app;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.view.View;
-import android.widget.Toast;
 
 public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_activity);
     }
 
-    public void onClick(View view) {
-        int id  = view.getId();
-        switch (id){
-            case R.id.btn_broad:
-            {
-                Intent broad = new Intent("info.update");
-                sendBroadcast(broad);
-            }
-                break;
-            default:
-                break;
-        }
-    }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        IntentFilter filter = new IntentFilter("info.update");
-        registerReceiver(receiver,filter);
-    }
-
-    BroadcastReceiver receiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            Toast.makeText(context,"update",Toast.LENGTH_SHORT).show();
-        }
-    };
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        unregisterReceiver(receiver);
-    }
-
+//    public void onClick(View view) {
+//        int id  = view.getId();
+//        switch (id){
+//            case R.id.btn_broad:
+//            {
+//                Intent broad = new Intent("info.update");
+//                sendBroadcast(broad);
+//            }
+//                break;
+//            default:
+//                break;
+//        }
+//    }
+//
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        IntentFilter filter = new IntentFilter("info.update");
+//        registerReceiver(receiver,filter);
+//    }
+//
+//    BroadcastReceiver receiver = new BroadcastReceiver() {
+//        @Override
+//        public void onReceive(Context context, Intent intent) {
+//            Toast.makeText(context,"update",Toast.LENGTH_SHORT).show();
+//        }
+//    };
+//
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        unregisterReceiver(receiver);
+//    }
 
 }
